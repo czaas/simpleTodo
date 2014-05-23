e21184fffa49e61c5400830e88283226590386dc
czaas <cameronzaas@gmail.com>
2014-05-23 01:29:14 -0700

    todo list working

--- js/angAPP.js
+++ js/angAPP.js
@@ -1,10 +1,15 @@
 // a todo list
 
 function todoCtrl($scope){
-	$scope.totalTodo= 6;
 
 	$scope.todos = [
 		{text: "Learn AngularJS", done: false},
 		{text: "Build an app", done: false}
-	]
+	];
+
+	$scope.addTodo = function(){
+		$scope.todos.push({text: $scope.formTodoText, done: false});
+		$scope.formTodoText = " "
+	}
+
 }
