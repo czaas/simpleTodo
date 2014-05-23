668fa71286589ebce11afea1bf2ca31a0e003828
czaas <cameronzaas@gmail.com>
2014-05-23 02:07:13 -0700

    More functionality

--- js/angAPP.js
+++ js/angAPP.js
@@ -6,10 +6,20 @@ function todoCtrl($scope){
 		{text: "Learn AngularJS", done: false},
 		{text: "Build an app", done: false}
 	];
+	
+	$scope.getTotalTodos = function(){
+		return $scope.todos.length;
+	};
+
+	$scope.clearFin = function(){
+		$scope.todos = _.filter($scope.todos, function(todo){
+			return !todo.done;
+		});
+	};
 
 	$scope.addTodo = function(){
 		$scope.todos.push({text: $scope.formTodoText, done: false});
-		$scope.formTodoText = " "
-	}
+		$scope.formTodoText = " ";
+	};
 
 }
